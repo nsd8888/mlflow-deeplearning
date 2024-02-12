c03a2ec2e66648263d9cb7912269a07f09ff5c1b
nsd8888 <nileshdalagade88@gmail.com>
2024-02-12 10:30:16 +0530

    updating artifact_downloader

--- artifact_download.py
+++ artifact_download.py
@@ -2,8 +2,8 @@ import mlflow
 from argparse import ArgumentParser
 
 par = ArgumentParser()
-par.add_argument("--model_uri", type=str)
-par.add_argument("--model_version", type=int)
+par.add_argument("--model_uri", type = str)
+par.add_argument("--model_version", type = int)
 par.add_argument("--MLFLOW_TRACKING_URI")
 par.add_argument("--MLFLOW_TRACKING_USERNAME")
 par.add_argument("--MLFLOW_TRACKING_PASSWORD")
@@ -15,5 +15,5 @@ MLFLOW_TRACKING_USERNAME = args.MLFLOW_TRACKING_USERNAME
 MLFLOW_TRACKING_PASSWORD = args.MLFLOW_TRACKING_PASSWORD
 
 
-model_uri = f"models:/{args.model_uri}/{args.model_version}"
+model_uri = f"models:/{str(args.model_uri)}/{str(args.model_version)}"
 mlflow.artifacts.download_artifacts(dst_path = "artifacts/", artifact_uri = model_uri)
