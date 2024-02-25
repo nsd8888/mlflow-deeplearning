16ba3722e4beec16d7b6bb438bf155ae45de3140
nsd8888 <nileshdalagade88@gmail.com>
2024-02-25 23:04:32 +0530

    updating env vars

--- artifact_download.py
+++ artifact_download.py
@@ -1,14 +1,17 @@
 import mlflow
 from argparse import ArgumentParser
-
+import os
 par = ArgumentParser()
 par.add_argument("--model_uri", type = str)
 par.add_argument("--model_version", type = int)
-par.add_argument("--MLFLOW_TRACKING_URI", type = str)
-
 args = par.parse_args()
-MLFLOW_TRACKING_URI = args.MLFLOW_TRACKING_URI
-mlflow.set_tracking_uri(args.MLFLOW_TRACKING_URI)
+
+MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
+MLFLOW_TRACKING_USERNAME = os.getenv("MLFLOW_TRACKING_USERNAME")
+MLFLOW_TRACKING_PASSWORD = os.getenv("MLFLOW_TRACKING_PASSWORD")
+
+# MLFLOW_TRACKING_URI = MLFLOW_TRACKING_URI
+# mlflow.set_tracking_uri(args.MLFLOW_TRACKING_URI)
 
 lst = ["Ordinal_encode_bp.pkl", "Ordinal_encode_cho.pkl", "Onehot_encode_sex.pkl", "Std.pkl", "Label_encode.pkl", "requirements.txt"]
 for i in lst:
